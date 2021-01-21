c56b46a568a5fc226be35b83f062687e8973f17a
Brendan Hasz <winsto99@gmail.com>
2021-01-20 18:32:02 -0600

    Update stop_fastai_vm.sh

--- stop_fastai_vm.sh
+++ stop_fastai_vm.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-# Script to deallocate a Azure GPU VM instance
+# Script to deallocate a Azure GPU VM instance and delete a resource group
 
 # Inputs
-read -p "Azure VM name / resource group (default: ${USER}-fastai): " vminput
+read -p "Azure VM name (default: ${USER}-fastai): " vminput
 vmname=${vminput:="${USER}-fastai"}
+read -p "Azure resource group (default: eastus-fastai-${USER}-rg): " rginput
+rgname=${rginput:="eastus-fastai-${USER}-rg"}
 
 # Deallocate the VM
 az vm deallocate -g $vmname -n $vmname
 
 # Delete the resource group
 az group delete -n $vmname
-
